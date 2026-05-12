3627937a84abf558b6b6674a6186f6b2b5157707
UGO COELHO <2024102070@academicougb.com.br>
2026-05-12 20:22:11 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Controllers/AcademicoController.cs
@@ -0,0 +1,30 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class AcademicoController : Controller
+
+    {
+        private static List<Aluno> alunos = new List<Aluno>()
+        {
+            new Aluno { Id = 1, Nome = "UgoLamana", Email = "Coelhou@gmail.com", DataNascimento = new DateTime(2004,07,13)}
+        };
+        public IActionResult Index()
+        {
+            return View(alunos);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Aluno aluno)
+        {
+            aluno.Id = alunos.Count + 1;
+            alunos.Add(aluno);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Academico/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Academico.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        
+        public string Nome { get; set; }
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+        [DisplayName("Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
